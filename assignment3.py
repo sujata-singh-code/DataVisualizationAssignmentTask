c16c8070fe8bcd219b473c4176866b0707271a4b
sujata-singh-code <inghshweta.143@gmail.com>
2024-11-06 14:18:28 +0530

    Create assignment3.py

---
+++ assignment3.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
+import matplotlib.pyplot as plt
+import seaborn as sns
+
